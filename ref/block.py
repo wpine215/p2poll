a720875c31f9eb771664cd87af1b2cfd20d5e2fe
wpine215 <wpine215@gmail.com>
2018-11-10 12:28:31 -0500

    Added data fields in Block class for polls and voters

--- ref/block.py
+++ ref/block.py
@@ -1,18 +1,20 @@
 import hashlib as hasher
 
 class Block:
-	def __init__(self, index, timestamp, voteData, pollData = None, voteData = None, previous_hash):
+	def __init__(self, index, timestamp, previous_hash, voteData, pollData = None, voterData = None):
 		self.index = index
 		self.timestamp = timestamp
+		self.previous_hash = previous_hash
+
 		self.votes = voteData
 		self.polls = pollData
-		self.voters = voteData
-		self.previous_hash = previous_hash
+		self.voters = voterData
+
 		self.hash = self.hash_block()
 
 	def hash_block(self):
 		sha = hasher.sha256()
-		sha.update((str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)).encode('utf-8'))
+		sha.update((str(self.index) + str(self.timestamp) + str(self.votes) + str(self.polls) + str(self.voters) + str(self.previous_hash)).encode('utf-8'))
 		return sha.hexdigest()
 
 
