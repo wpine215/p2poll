2af09480c7106f2c99f2eef0ed65a80dd2d6278e
Jeff <jeffrey.kl.hui@gmail.com>
2018-11-10 23:27:17 -0500

    before session integration

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,23 @@
 from flask import Flask, request, jsonify
 import json
+from twilio.rest import Client
+from random import *
+from creds import *
+
+account_sid = creds()[0]
+auth_token = creds()[1]
+client = Client(account_sid, auth_token)
+ 
+# global auth?Code
+# authCode = -1
+
+# def setAuthCode(code):
+# 	global authCode
+# 	authcode = code
+
+# def getAuthCode():
+# 	global authCode
+# 	return authCode
 
 app = Flask(__name__)
 
@@ -9,20 +27,31 @@ def index():
 
 @app.route('/lookup')
 def lookup():
-	voteid = request.args.get('voteid', default = 1, type = str)
+	voteid = request.args.get('voteid', default = 1, type = int)
 	if voteid == 1:
 		return jsonify({'date': 123, 'voted_for': 456})
 	return jsonify({'voteid': voteid})
 
-@app.route('/vote', methods=['GET','POST'])
+@app.route('/twofact', methods=['POST'])
+def twofact():
+	setAuthCode(randint(10000,99999)) 
+	msgBody = "Your Two Factor Auth Code is: " + str(authCode)
+	rec = '+18184371804'
+	# message = client.messages.create(body=msgBody,from_='+16178556948',to=rec)
+	print(msgBody)
+	return "Hello"
+
+@app.route('/vote')
 def vote():
-	if request.method == 'POST':
-		# call twillio api
-		pass
+	print(authCode)
+	token = request.args.get('token', default = 0, type = int)
+	if token != authCode: 
+		return jsonify({'auth':False})
 	else:
-		pass
-
-
+		return jsonify({'auth':True})
+			
 @app.route('/login',methods=['GET','POST'])
 def login():
-	username = request.args.get(''
+	username = request.args.get('')
+
+
