43f6eed90cdc96c269865b30afc8397e42a22924
wpine215 <wpine215@gmail.com>
2018-11-10 23:58:46 -0500

    Added basic blockchain example

---
+++ example/blockchain/server.py
@@ -0,0 +1,140 @@
+from urllib.parse import urlparse
+
+import requests
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+
+from blockchain import *
+
+# Instantiate the Node
+app = Flask(__name__)
+CORS(app)
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+@app.route('/configure')
+def configure():
+    return render_template('./configure.html')
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.form
+
+    # Validate submitted fields for completion
+    required = ['sender_address', 'recipient_address', 'amount', 'signature']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    transaction_result = blockchain.submit_transaction(values['sender_address'], values['recipient_address'], values['amount'], values['signature'])
+
+    if transaction_result == False:
+        response = {'message': 'Invalid Transaction!'}
+        return jsonify(response), 406
+    else:
+        response = {'message': 'Transaction will be added to block '+ str(transaction_result)}
+        return jsonify(response), 201
+
+@app.route('/transactions/get', methods=['GET'])
+def get_transactions():
+    #Get transactions from transactions pool
+    transactions = blockchain.transactions
+
+    response = {'transactions': transactions}
+    return jsonify(response), 200
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # We run the proof of work algorithm to get the next proof
+    last_block = blockchain.chain[-1]
+    nonce = blockchain.proof_of_work()
+
+    # We must receive a reward for finding the proof.
+    blockchain.submit_transaction(sender_address=MINING_SENDER, recipient_address=blockchain.node_id, value=MINING_REWARD, signature="")
+
+    # Forge the new block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.create_block(nonce, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'block_number': block['block_number'],
+        'transactions': block['transactions'],
+        'nonce': block['nonce'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.form
+    nodes = values.get('nodes').replace(" ", "").split(',')
+
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': [node for node in blockchain.nodes],
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/get', methods=['GET'])
+def get_nodes():
+    nodes = list(blockchain.nodes)
+    response = {'nodes': nodes}
+    return jsonify(response), 200
+
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='127.0.0.1', port=port)
+
+
+
+
+
+
+
+
---
+++ example/blockchain_client/vote.py
@@ -0,0 +1,51 @@
+'''
+title           : vote.py
+description     : A blockchain client implemenation, with the following features
+                  - Wallets generation using Public/Private key encryption (based on RSA algorithm)
+                  - Generation of transactions with RSA encryption      
+author          : Adil Moujahid
+date_created    : 20180212
+date_modified   : 20180309
+version         : 0.3
+usage           : python blockchain_client.py
+                  python blockchain_client.py -p 8080
+                  python blockchain_client.py --port 8080
+python_version  : 3.6.1
+Comments        : Wallet generation and transaction signature is based on [1]
+References      : [1] https://github.com/julienr/ipynb_playground/blob/master/bitcoin/dumbcoin/dumbcoin.ipynb
+'''
+
+from collections import OrderedDict
+
+import binascii
+
+import Crypto
+import Crypto.Random
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+
+class Transaction:
+
+    def __init__(self, sender_address, sender_private_key, recipient_address, value):
+        self.sender_address = sender_address
+        self.sender_private_key = sender_private_key
+        self.recipient_address = recipient_address
+        self.value = value
+
+    def __getattr__(self, attr):
+        return self.data[attr]
+
+    def to_dict(self):
+        return OrderedDict({'sender_address': self.sender_address,
+                            'recipient_address': self.recipient_address,
+                            'value': self.value})
+
+    def sign_transaction(self):
+        """
+        Sign transaction with private key
+        """
+        private_key = RSA.importKey(binascii.unhexlify(self.sender_private_key))
+        signer = PKCS1_v1_5.new(private_key)
+        h = SHA.new(str(self.to_dict()).encode('utf8'))
+        return binascii.hexlify(signer.sign(h)).decode('ascii')
